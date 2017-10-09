85cc83d7dc6e4c79e6a4f0c51d3a6bc5a5a2f9d1
sergotail <sergotail@mail.ru>
2017-10-10 00:17:41 +0300

    hw_2 done, all tests passed

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -1,39 +1,59 @@
 #include "MapBasedGlobalLockImpl.h"
 
-#include <mutex>
-
 namespace Afina {
-namespace Backend {
+	namespace Backend {
 
-// See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) {
-    std::unique_lock<std::mutex> guard(_lock);
-    return false;
-}
+		// See MapBasedGlobalLockImpl.h
+		bool MapBasedGlobalLockImpl::Put(std::string const & key, std::string const & value) {
+			Delete(key);
+			pushFront(key, value);
+			eraseLRU();
+			return true;
+		}
 
-// See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::string &value) {
-    std::unique_lock<std::mutex> guard(_lock);
-    return false;
-}
+		// See MapBasedGlobalLockImpl.h
+		bool MapBasedGlobalLockImpl::PutIfAbsent(std::string const & key, std::string const & value) {
+			if (!isKeyExists(key)) {
+				pushFront(key, value);
+				eraseLRU();
+				return true;
+			}
+			return false;
+		}
 
-// See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) {
-    std::unique_lock<std::mutex> guard(_lock);
-    return false;
-}
+		// See MapBasedGlobalLockImpl.h
+		bool MapBasedGlobalLockImpl::Set(std::string const & key, std::string const & value) {
+			if (isKeyExists(key)) {
+				Delete(key);
+				pushFront(key, value);
+				return true;
+			}
+			return false;
+		}
 
-// See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Delete(const std::string &key) {
-    std::unique_lock<std::mutex> guard(_lock);
-    return false;
-}
+		// See MapBasedGlobalLockImpl.h
+		bool MapBasedGlobalLockImpl::Delete(std::string const & key) {
+			std::unique_lock<std::mutex> guard(_lock);
+			auto it = _hash_table.find(key);
+			if (it != _hash_table.end()) {
+				_items_list.erase(it->second);
+				_hash_table.erase(it);
+				return true;
+			}
+			return false;
+		}
 
-// See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const {
-    std::unique_lock<std::mutex> guard(*const_cast<std::mutex *>(&_lock));
-    return false;
-}
+		// See MapBasedGlobalLockImpl.h
+		bool MapBasedGlobalLockImpl::Get(std::string const & key, std::string & value) const {
+			std::unique_lock<std::mutex> guard(*const_cast<std::mutex *>(&_lock));
+			auto it = _hash_table.find(key);
+			if (it != _hash_table.end()) {
+				_items_list.splice(_items_list.begin(), _items_list, it->second);
+				value = it->second->second;
+				return true;
+			}
+			return false;
+		}
 
-} // namespace Backend
+	} // namespace Backend
 } // namespace Afina
--- src/storage/MapBasedGlobalLockImpl.h
+++ src/storage/MapBasedGlobalLockImpl.h
@@ -1,49 +1,67 @@
 #ifndef AFINA_STORAGE_MAP_BASED_GLOBAL_LOCK_IMPL_H
 #define AFINA_STORAGE_MAP_BASED_GLOBAL_LOCK_IMPL_H
 
-#include <map>
+#include <unordered_map>
+#include <list>
 #include <mutex>
 #include <string>
 
 #include <afina/Storage.h>
 
 namespace Afina {
-namespace Backend {
+	namespace Backend {
 
-/**
- * # Map based implementation with global lock
- *
- *
- */
-class MapBasedGlobalLockImpl : public Afina::Storage {
-public:
-    MapBasedGlobalLockImpl(size_t max_size = 1024) : _max_size(max_size) {}
-    ~MapBasedGlobalLockImpl() {}
+		/**
+		* # Map based implementation with global lock
+		*
+		*
+		*/
+		class MapBasedGlobalLockImpl : public Afina::Storage {
+		public:
+			MapBasedGlobalLockImpl(std::size_t const max_size = 1024) : 
+				_max_size(max_size) {}
+			~MapBasedGlobalLockImpl() {}
 
-    // Implements Afina::Storage interface
-    bool Put(const std::string &key, const std::string &value) override;
+			// Implements Afina::Storage interface
+			bool Put(std::string const & key, std::string const & value) override;
 
-    // Implements Afina::Storage interface
-    bool PutIfAbsent(const std::string &key, const std::string &value) override;
+			// Implements Afina::Storage interface
+			bool PutIfAbsent(std::string const & key, std::string const & value) override;
 
-    // Implements Afina::Storage interface
-    bool Set(const std::string &key, const std::string &value) override;
+			// Implements Afina::Storage interface
+			bool Set(std::string const & key, std::string const & value) override;
 
-    // Implements Afina::Storage interface
-    bool Delete(const std::string &key) override;
+			// Implements Afina::Storage interface
+			bool Delete(std::string const & key) override;
 
-    // Implements Afina::Storage interface
-    bool Get(const std::string &key, std::string &value) const override;
+			// Implements Afina::Storage interface
+			bool Get(std::string const & key, std::string & value) const override;
 
-private:
-    std::mutex _lock;
+		private:
+			mutable std::mutex _lock;
+			std::size_t const _max_size;
+			mutable std::list<std::pair<std::string, std::string>> _items_list;
+			std::unordered_map<std::string, decltype(_items_list.begin())> _hash_table;
+			void eraseLRU() {
+				std::unique_lock<std::mutex> guard(_lock);
+				while (_hash_table.size() > _max_size) {
+					auto lru = _items_list.end();
+					_hash_table.erase((--lru)->first);
+					_items_list.pop_back();
+				}
+			}
+			bool isKeyExists(std::string const & key) const {
+				std::unique_lock<std::mutex> guard(_lock);
+				return _hash_table.count(key) > 0;
+			}
+			void pushFront(std::string const & key, std::string const & value) {
+				std::unique_lock<std::mutex> guard(_lock);
+				_items_list.push_front(make_pair(key, value));
+				_hash_table.insert(make_pair(key, _items_list.begin()));
+			}
+		};
 
-    size_t _max_size;
-
-    std::map<std::string, std::string> _backend;
-};
-
-} // namespace Backend
+	} // namespace Backend
 } // namespace Afina
 
 #endif // AFINA_STORAGE_MAP_BASED_GLOBAL_LOCK_IMPL_H
